2184d1b34c9ac3c22cfd53e4ec5f3845468f7458
rlzidani <rlzidani@comcast.net>
2018-07-18 04:12:33 +0530

    Add test

---
+++ my-app1/src/test/java/HelloWorld.java
@@ -0,0 +1,9 @@
+ // The classic Hello World! program //1
+ class HelloWorld { //2
+	 public static void main(String[] args) { //3
+		 //Write Hello World to the terminal window
+		 system.out.println("Hello World!); //4
+	 } //5
+
+ } //6
+
